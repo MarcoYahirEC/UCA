3fc75c568819fa74810147fc3b196b7c823d8aee
MarcoYahirEC <156841959+MarcoYahirEC@users.noreply.github.com>
2024-03-07 10:09:44 -0600

    se corrigio js

--- JS/Finales.js
+++ JS/Finales.js
@@ -1,7 +1,7 @@
 function pedirFinal1(){
     const pedido = "Final 1";
-    const respuesta = "tu pedido de"+ pedido + "ha sido enviado";
-    const elementoRespuesta = document.getElementById("Final 1");
+    const final1 = "tu pedido de"+ pedido + "ha sido enviado";
+    const elementoFinal1 = document.getElementById("Final 1");
     elementoRespuesta.textContent = respuesta;
     elementoRespuesta.style.display="block";
 }
