a9cd4998c1b0925affb71a756e4d7ced9ea69bbc
MarcoYahirEC <156841959+MarcoYahirEC@users.noreply.github.com>
2024-03-07 10:48:59 -0600

    aaaaaaaaa

--- JS/Finales.js
+++ JS/Finales.js
@@ -2,3 +2,7 @@ function pedirFinal1(){
     var div = document.getElementById("finales");
     div.innerHTML = "<p>la guerra finalizo y eres libre</p>";
   }
+  function pedirFinal2(){
+    var div = document.getElementById("finales");
+    div.innerHTML = "<p>Luchas en el campo de guerra y mueres ademas eres enviado hacia el inframundo</p>";
+  }
