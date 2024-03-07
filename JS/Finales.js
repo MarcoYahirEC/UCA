9fd2e60a8f8dc05f6f15572994b788226627e699
MarcoYahirEC <156841959+MarcoYahirEC@users.noreply.github.com>
2024-03-07 10:53:00 -0600

    aaaaaaa

--- JS/Finales.js
+++ JS/Finales.js
@@ -6,3 +6,7 @@ function pedirFinal1(){
     var div = document.getElementById("finales");
     div.innerHTML = "<p>Luchas en el campo de guerra y mueres ademas eres enviado hacia el inframundo</p>";
   }
+  function pedirFinal3(){
+    var div = document.getElementById("finales");
+    div.innerHTML = "<p>Vas a la guerra y sales victorioso ademas recibes riquezas y el reconocimiento de que fuiste un guerrero noble</p>";
+  }
