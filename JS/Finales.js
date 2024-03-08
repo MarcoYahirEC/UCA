a2b387a276b200f0c82508526951faddb5d849fb
MarcoYahirEC <156841959+MarcoYahirEC@users.noreply.github.com>
2024-03-08 08:33:42 -0600

    se modifico los finales

--- JS/Finales.js
+++ JS/Finales.js
@@ -1,12 +1,12 @@
 function pedirFinal1(){
     var div = document.getElementById("finales");
-    div.innerHTML = "<p>la guerra finalizo y eres libre</p>";
+    div.innerHTML = "<p>Despues de años de conflicto , los lideres de atenas y esparta se reunen en un intento desesperado por poner fin a la guerra . despues de arduas negociaciones , ambas partes acuerdan un tratado de paz.</p>";
   }
   function pedirFinal2(){
     var div = document.getElementById("finales");
-    div.innerHTML = "<p>Luchas en el campo de guerra y mueres ademas eres enviado hacia el inframundo</p>";
+    div.innerHTML = "<p>en un giro inesperado de los acontecimientos , las fuerzas espartanas , lideradas por el habil estratega leonidas , logran una impresionante victoria sobre el ejercito ateniense</p>";
   }
   function pedirFinal3(){
     var div = document.getElementById("finales");
-    div.innerHTML = "<p>Vas a la guerra y sales victorioso ademas recibes riquezas y el reconocimiento de que fuiste un guerrero noble</p>"
+    div.innerHTML = "<p>Mientras la guerra entre atenas y esprta alcanza su punto algido , una amenaza emerge en forma de un poderoso imperio persa que busca conquistar grecia.</p>"
   }
